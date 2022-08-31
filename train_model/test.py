1e010fcc4d80ecb825a73368ad3031cb3a3182f8
Alpacz <rumrumtot@hotmail.com>
2022-08-31 11:55:26 +0700

    -

--- train_model/test.py
+++ train_model/test.py
@@ -2,6 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-df =pd.read_csv('sen_word.csv',sep='\t',names=['text','sentiment'],header=None)
+df = pd.read_csv('sen_word.csv', sep='\t', names=['text', 'sentiment'], header=None)
 
 print(df)
