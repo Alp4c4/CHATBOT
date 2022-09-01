bc00dcfb46eb2230425267c111e5c8c8ef48f6cd
Alpacz <rumrumtot@hotmail.com>
2022-09-01 09:51:59 +0700

    -update

--- train_model/sentiment.py
+++ train_model/sentiment.py
@@ -1,26 +1,52 @@
-from nltk import NaiveBayesClassifier as nbc
-from pythainlp.tokenize import word_tokenize
-import codecs
-from itertools import chain
-# pos.txt
-with codecs.open('pos.txt', encoding="utf-8") as f:
-    lines = f.readlines()
-listpos=[e.strip() for e in lines]
-del lines
-f.close() # ปิดไฟล์
-#neg.txt
-with codecs.open('neg.txt', encoding="utf-8") as f:
-    lines = f.readlines()
-listneg=[e.strip() for e in lines]
-f.close() # ปิดไฟล์
-pos1=['pos']*len(listpos)
-neg1=['neg']*len(listneg)
-training_data = list(zip(listpos,pos1)) + list(zip(listneg,neg1))
-vocabulary = set(chain(*[word_tokenize(i[0].lower()) for i in training_data]))
-feature_set = [({i:(i in word_tokenize(sentence.lower())) for i in vocabulary},tag) for sentence, tag in training_data]
-classifier = nbc.train(feature_set)
-while True:
-	test_sentence = input('\nข้อความ : ')
-	featurized_test_sentence =  {i:(i in word_tokenize(test_sentence.lower())) for i in vocabulary}
-	print("test_sent:",test_sentence)
-	print("tag:",classifier.classify(featurized_test_sentence)) # ใช้โมเดลที่ train ประมวลผล
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import confusion_matrix,classification_report
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from wordcloud import WordCloud, STOPWORDS
+from pythainlp import word_tokenize
+from pythainlp.corpus.common import thai_stopwords
+from pythainlp import word_tokenize
+df = pd.read_csv('sen.txt', sep='\t', names=['text', 'sentiment'], header=None)
+thai_stopwords=list(thai_stopwords())
+def text_process(text):
+    final = "".join(u for u in text if u not in ("?", ".", ";", ":", "!", '"', "ๆ", "ฯ"))
+    final = word_tokenize(final)
+    final = " ".join(word for word in final)
+    final = " ".join(word for word in final.split() 
+                     if word.lower not in thai_stopwords)
+    return final
+df['text_tokens'] = df['text'].apply(text_process)
+
+# train_model
+X = df[['text_tokens']]
+y = df['sentiment']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.01, random_state=92)
+
+
+cvec = CountVectorizer(analyzer=lambda x:x.split(' '))
+cvec.fit_transform(X_train['text_tokens'])
+cvec.vocabulary_
+
+
+train_bow = cvec.transform(X_train['text_tokens'])
+pd.DataFrame(train_bow.toarray(), columns=cvec.get_feature_names(), index=X_train['text_tokens'])
+
+lr = LogisticRegression()
+lr.fit(train_bow, y_train)
+
+test_bow = cvec.transform(X_test['text_tokens'])
+test_predictions = lr.predict(test_bow)
+print(classification_report(test_predictions, y_test))
+
+# my_text = input('\nข้อความ : ')
+# my_tokens = text_process(my_text)
+# my_bow = cvec.transform(pd.Series([my_tokens]))
+# my_predictions = lr.predict(my_bow)
+# my_predictions
+
+	
+    
+    
--- train_model/test.py
+++ train_model/test.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-
-
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import confusion_matrix,classification_report
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 from wordcloud import WordCloud, STOPWORDS
 from pythainlp import word_tokenize
 from pythainlp.corpus.common import thai_stopwords
@@ -17,14 +19,34 @@ def text_process(text):
                      if word.lower not in thai_stopwords)
     return final
 df['text_tokens'] = df['text'].apply(text_process)
-df
 
-df_pos = df[df['sentiment'] == 'pos']
-pos_word_all = " ".join(text for text in df_pos['text_tokens'])
-reg = r"[ก-๙a-zA-Z']+"
-fp = 'THSarabunNew.ttf'
-wordcloud = WordCloud(stopwords=thai_stopwords, background_color = 'white', max_words=2000, height = 2000, width=4000, font_path=fp, regexp=reg).generate(pos_word_all)
-plt.figure(figsize = (16,8))
-plt.imshow(wordcloud)
-plt.axis('off')
-plt.show()
+# train_model
+X = df[['text_tokens']]
+y = df['sentiment']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+
+
+cvec = CountVectorizer(analyzer=lambda x:x.split(' '))
+cvec.fit_transform(X_train['text_tokens'])
+cvec.vocabulary_
+
+
+train_bow = cvec.transform(X_train['text_tokens'])
+pd.DataFrame(train_bow.toarray(), columns=cvec.get_feature_names(), index=X_train['text_tokens'])
+
+lr = LogisticRegression()
+lr.fit(train_bow, y_train)
+
+test_bow = cvec.transform(X_test['text_tokens'])
+test_predictions = lr.predict(test_bow)
+print(classification_report(test_predictions, y_test))
+
+# my_text = input('\nข้อความ : ')
+# my_tokens = text_process(my_text)
+# my_bow = cvec.transform(pd.Series([my_tokens]))
+# my_predictions = lr.predict(my_bow)
+# my_predictions
+
+	
+    
+    
