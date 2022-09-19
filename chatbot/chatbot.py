66372a3259635d9f4049ee46dcd844aae7d01a62
Alpacz <rumrumtot@hotmail.com>
2022-09-19 16:21:15 +0700

    Update chatbot.py

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,7 +1,24 @@
 import os 
-import dialogflow_v2 as dialogflow
+#-*-coding: utf-8 -*-
+##from __future__ import absolute_import
+###
+from flask import Flask, jsonify, render_template, request
+import json
+import numpy as np
+import pandas as pd
+import geopy.distance as ps
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage,ImageSendMessage, StickerSendMessage, AudioSendMessage
+)
+from linebot.models.template import *
+from linebot import (
+    LineBotApi, WebhookHandler
+)
 
-session_client = dialogflow.SessionClient()
+app = Flask(__name__)
+
+lineaccesstoken = 'CFGppk8AuPQl705iQwgP8cZE9Gn4CumoTp7BYNvKnbOtf9zSqOkRyFgMgz9fM/U58jsG2LyPB5ds7R99GHZde3y95T5988EWSbLEU0upcB6c12HhIYf4V+d+4oki21kgciXeA0fn5CxPienZ7e5UVwdB04t89/1O/w1cDnyilFU='
+line_bot_api = LineBotApi(lineaccesstoken)
 # def chatbot():
     
 
