3bc746a64d09916c2c886e0fc5dad98ea61b0a1b
Alpacz <rumrumtot@hotmail.com>
2022-09-19 19:55:21 +0700

    -testupdate

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,25 +1,11 @@
-import os 
-#-*-coding: utf-8 -*-
-##from __future__ import absolute_import
-###
-from flask import Flask, jsonify, render_template, request
-import json
-import numpy as np
-import pandas as pd
-import geopy.distance as ps
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage,ImageSendMessage, StickerSendMessage, AudioSendMessage
-)
-from linebot.models.template import *
-from linebot import (
-    LineBotApi, WebhookHandler
-)
+from flask import Flask
 
 app = Flask(__name__)
 
-lineaccesstoken = 'CFGppk8AuPQl705iQwgP8cZE9Gn4CumoTp7BYNvKnbOtf9zSqOkRyFgMgz9fM/U58jsG2LyPB5ds7R99GHZde3y95T5988EWSbLEU0upcB6c12HhIYf4V+d+4oki21kgciXeA0fn5CxPienZ7e5UVwdB04t89/1O/w1cDnyilFU='
-line_bot_api = LineBotApi(lineaccesstoken)
-# def chatbot():
-    
 
+@app.route('/')
+def home():
+    return "Flask heroku app."
 
+if __name__ == '__main__':
+    app.run()
