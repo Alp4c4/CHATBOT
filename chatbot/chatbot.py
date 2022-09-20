87dc6950038889d2dd9c154edf6f62c5463f2da5
Alpacz <rumrumtot@hotmail.com>
2022-09-20 19:15:15 +0700

    -update

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -2,10 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def home():
-    return "Flask heroku app."
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
 if __name__ == '__main__':
     app.run()
