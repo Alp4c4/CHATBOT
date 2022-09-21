23974d6e6849ecfb6ba13d32cbce514808f5f3d4
Alpacz <rumrumtot@hotmail.com>
2022-09-21 17:22:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,18 +6,18 @@ from flask import Flask, jsonify, render_template, request
 import json
 import numpy as np
 
-# from linebot.models import (
-#     MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage,ImageSendMessage, StickerSendMessage, AudioSendMessage
-# )
-# from linebot.models.template import *
-# from linebot import (
-#     LineBotApi, WebhookHandler
-# )
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage,ImageSendMessage, StickerSendMessage, AudioSendMessage
+)
+from linebot.models.template import *
+from linebot import (
+    LineBotApi, WebhookHandler
+)
 
 app = Flask(__name__)
 
-# lineaccesstoken = 'CFGppk8AuPQl705iQwgP8cZE9Gn4CumoTp7BYNvKnbOtf9zSqOkRyFgMgz9fM/U58jsG2LyPB5ds7R99GHZde3y95T5988EWSbLEU0upcB6c12HhIYf4V+d+4oki21kgciXeA0fn5CxPienZ7e5UVwdB04t89/1O/w1cDnyilFU='
-# line_bot_api = LineBotApi(lineaccesstoken)
+lineaccesstoken = 'CFGppk8AuPQl705iQwgP8cZE9Gn4CumoTp7BYNvKnbOtf9zSqOkRyFgMgz9fM/U58jsG2LyPB5ds7R99GHZde3y95T5988EWSbLEU0upcB6c12HhIYf4V+d+4oki21kgciXeA0fn5CxPienZ7e5UVwdB04t89/1O/w1cDnyilFU='
+line_bot_api = LineBotApi(lineaccesstoken)
 
 ###################################
 @app.route('/')
