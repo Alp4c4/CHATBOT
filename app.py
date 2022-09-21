b775034f0e87b030b38132de946eb0a7f22cb341
Alpacz <rumrumtot@hotmail.com>
2022-09-21 16:54:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask,request,render_template,url_for
+#!/usr/bin/python
+#-*-coding: utf-8 -*-
+##from __future__ import absolute_import
+###
+from flask import Flask, jsonify, render_template, request
+import json
+import numpy as np
 
-app=Flask(__name__)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage,ImageSendMessage, StickerSendMessage, AudioSendMessage
+)
+from linebot.models.template import *
+from linebot import (
+    LineBotApi, WebhookHandler
+)
 
-@app.route('/')
-def hello_world():
-    return 'Hello,World!'
+app = Flask(__name__)
 
-
-    app.run()
+lineaccesstoken = 'CFGppk8AuPQl705iQwgP8cZE9Gn4CumoTp7BYNvKnbOtf9zSqOkRyFgMgz9fM/U58jsG2LyPB5ds7R99GHZde3y95T5988EWSbLEU0upcB6c12HhIYf4V+d+4oki21kgciXeA0fn5CxPienZ7e5UVwdB04t89/1O/w1cDnyilFU='
+line_bot_api = LineBotApi(lineaccesstoken)
