72030803a4eac8fc59522b5f6ed0c8f7be88d6df
Alpacz <rumrumtot@hotmail.com>
2022-10-08 11:56:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
 from flask import Flask, jsonify, render_template, request
 import json
 import numpy as np
-# import Usesentiment
+import Usesentiment
 # import Clincaldepression_test
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage,ImageSendMessage, StickerSendMessage, AudioSendMessage
