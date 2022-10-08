2388a4c4b6abc1525703d42090f088ba8f963bef
Alpacz <rumrumtot@hotmail.com>
2022-10-08 17:58:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import json
 import numpy as np
 import os 
 ###import sentiment analy
-# import UseSentiment 
+import UseSentiment 
 # import ClinCaldepression_test
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage,ImageSendMessage, StickerSendMessage, AudioSendMessage
@@ -26,51 +26,51 @@ line_bot_api = LineBotApi(lineaccesstoken)
 @app.route('/')
 def index():
     return "Hello World!"
-@app.route('/webhook', methods=['POST'])
-def callback():
-    json_line = request.get_json(force=False,cache=False)
-    json_line = json.dumps(json_line)
-    decoded = json.loads(json_line)
-    no_event = len(decoded['events'])
-    for i in range(no_event):
-        event = decoded['events'][i]
-        event_handle(event)
-    return '',200
+# @app.route('/webhook', methods=['POST'])
+# def callback():
+#     json_line = request.get_json(force=False,cache=False)
+#     json_line = json.dumps(json_line)
+#     decoded = json.loads(json_line)
+#     no_event = len(decoded['events'])
+#     for i in range(no_event):
+#         event = decoded['events'][i]
+#         event_handle(event)
+#     return '',200
 
 
-def event_handle(event):
-    print(event)
-    try:
-        userId = event['source']['userId']
-    except:
-        print('error cannot get userId')
-        return ''
+# def event_handle(event):
+#     print(event)
+#     try:
+#         userId = event['source']['userId']
+#     except:
+#         print('error cannot get userId')
+#         return ''
 
-    try:
-        rtoken = event['replyToken']
-    except:
-        print('error cannot get rtoken')
-        return ''
-    try:
-        msgId = event["message"]["id"]
-        msgType = event["message"]["type"]
-    except:
-        print('error cannot get msgID, and msgType')
-        sk_id = np.random.randint(1,17)
-        replyObj = StickerSendMessage(package_id=str(1),sticker_id=str(sk_id))
-        line_bot_api.reply_message(rtoken, replyObj)
-        return ''
+#     try:
+#         rtoken = event['replyToken']
+#     except:
+#         print('error cannot get rtoken')
+#         return ''
+#     try:
+#         msgId = event["message"]["id"]
+#         msgType = event["message"]["type"]
+#     except:
+#         print('error cannot get msgID, and msgType')
+#         sk_id = np.random.randint(1,17)
+#         replyObj = StickerSendMessage(package_id=str(1),sticker_id=str(sk_id))
+#         line_bot_api.reply_message(rtoken, replyObj)
+#         return ''
 
-    if msgType == "text":
-        msg = str(event["message"]["text"])
-        replyObj = TextSendMessage(text=msg)
-        line_bot_api.reply_message(rtoken, replyObj)
+#     if msgType == "text":
+#         msg = str(event["message"]["text"])
+#         replyObj = TextSendMessage(text=msg)
+#         line_bot_api.reply_message(rtoken, replyObj)
 
-    else:
-        sk_id = np.random.randint(1,17)
-        replyObj = StickerSendMessage(package_id=str(1),sticker_id=str(sk_id))
-        line_bot_api.reply_message(rtoken, replyObj)
-    return ''
+#     else:
+#         sk_id = np.random.randint(1,17)
+#         replyObj = StickerSendMessage(package_id=str(1),sticker_id=str(sk_id))
+#         line_bot_api.reply_message(rtoken, replyObj)
+#     return ''
 
 
 
