252cb6dbbedb507b06d8db43990d50bd3e0b97b1
Alpacz <rumrumtot@hotmail.com>
2022-10-08 12:56:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ line_bot_api = LineBotApi(lineaccesstoken)
 ###################################
 @app.route('/')
 def index():
-    return "Hello World! test "
+    return "Hello World!"
 @app.route('/webhook', methods=['POST'])
 def callback():
     json_line = request.get_json(force=False,cache=False)
