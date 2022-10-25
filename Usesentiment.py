f593f499ddfae4485c2b71df2dcf356e81195037
Alpacz <74106021+Alp4c4@users.noreply.github.com>
2022-10-25 21:44:07 +0700

    -

--- Usesentiment.py
+++ Usesentiment.py
@@ -19,35 +19,35 @@ classifier_f = open("model.pickle", "rb")
 classifier = pickle.load(classifier_f)
 classifier_f.close()
 
-#############################################
-# def text_process(text):
-#     final = "".join(u for u in text if u not in ("?", ".", ";", ":", "!", '"', "ๆ", "ฯ"))
-#     final = word_tokenize(final)
-#     final = " ".join(word for word in final)
-#     final = " ".join(word for word in final.split() 
-#                      if word.lower not in thai_stopwords)
-#     return final
-# df['text_tokens'] = df['text'].apply(text_process)
+############################################
+def text_process(text):
+    final = "".join(u for u in text if u not in ("?", ".", ";", ":", "!", '"', "ๆ", "ฯ"))
+    final = word_tokenize(final)
+    final = " ".join(word for word in final)
+    final = " ".join(word for word in final.split() 
+                     if word.lower not in thai_stopwords)
+    return final
+df['text_tokens'] = df['text'].apply(text_process)
 
-# train_model
-# X = df[['text_tokens']]
-# y = df['sentiment']
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.01,random_state=1)
+# # train_model
+X = df[['text_tokens']]
+y = df['sentiment']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.01,random_state=1)
 
-# cvec = CountVectorizer(analyzer=lambda x:x.split(' '))
-# cvec.fit_transform(X_train['text_tokens'])
-# cvec.vocabulary_
+cvec = CountVectorizer(analyzer=lambda x:x.split(' '))
+cvec.fit_transform(X_train['text_tokens'])
+cvec.vocabulary_
 
 
-# train_bow = cvec.transform(X_train['text_tokens'])
-# pd.DataFrame(train_bow.toarray(), columns=cvec.get_feature_names(), index=X_train['text_tokens'])
+train_bow = cvec.transform(X_train['text_tokens'])
+pd.DataFrame(train_bow.toarray(), columns=cvec.get_feature_names(), index=X_train['text_tokens'])
 
-# lr = LogisticRegression()f
-# train_lr=lr.fit(train_bow, y_train)
+lr = LogisticRegression()
+train_lr=lr.fit(train_bow, y_train)
 
-# test_bow = cvec.transform(X_test['text_tokens'])
-# test_predictions = lr.predict(test_bow)
-# print(classification_report(test_predictions, y_test))
+test_bow = cvec.transform(X_test['text_tokens'])
+test_predictions = lr.predict(test_bow)
+print(classification_report(test_predictions, y_test))
 
 def useSentiment(my_text):
     my_tokens = text_process(my_text)
@@ -60,7 +60,6 @@ def useSentiment(my_text):
     
     
 
-while True:
-    my_text = input('\nข้อความ : ')  
-    useSentiment(my_text)
-    break
+# while True:
+#     useSentiment(text)
+#     break
