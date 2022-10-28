98ac7cc13cefff712235e7725d60b1563c19781e
Alpacz <74106021+Alp4c4@users.noreply.github.com>
2022-10-28 13:12:35 +0700

    -

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from cgitb import handler
 from email import header
 from re import S
 from tkinter import scrolledtext
+from turtle import update
 from numpy import deprecate, round_
 
 from pkg_resources import ensure_directory
@@ -34,14 +35,14 @@ from linebot.models import (
 lineaccesstoken = 'CFGppk8AuPQl705iQwgP8cZE9Gn4CumoTp7BYNvKnbOtf9zSqOkRyFgMgz9fM/U58jsG2LyPB5ds7R99GHZde3y95T5988EWSbLEU0upcB6c12HhIYf4V+d+4oki21kgciXeA0fn5CxPienZ7e5UVwdB04t89/1O/w1cDnyilFU='
 line_bot_api = LineBotApi(lineaccesstoken)
 handler = WebhookHandler('cf3954fc20dc1f7221437b5f0b811e85')
-####################################
-# from random import randint
-# import firebase_admin
-# from firebase_admin import credentials
-# from firebase_admin import firestore 
-# # cred=credentials.Certificate("")
-# firebase_admin.initialize_app(cred)
 ###################################
+from random import randint
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore 
+cred=credentials.Certificate("depreesion-4eb38-firebase-adminsdk-r6lnp-402912a06a.json")
+firebase_admin.initialize_app(cred)
+##################################
 # question_for_test=[("ที่ผ่านมาคุณเป็นอย่างไรบ้าง รู้สึกมีเรื่องที่ไม่สบายใจหรือท้อแท้บ้างไหม"),("คุณรู้สึกเหนื่อยง่าย ไม่กระปรี้กระเปร่าบ้างไหม"),("คุณรู้สึกเบื่ออาหาร หรือกินมากเกินไปไหม"),("คุณรู้สึกว่าตัวเองหลับยาก หลับๆตื่นๆ หรือ หลับมากเกินไปไหม"),("คุณรู้สึกเบื่อบ้างไหม"),("คุณไม่มีสมาธิ เวลาทำงานที่ต้องใช้ความตั้งใจ เช่นการดูโทรทัศน์ หรือ อ่านหนังสือ ไหม"),("คุณรู้สึกว่าตัวเองพูดช้า ทำอะไรช้าลง จนคนอื่นสังเกตเห็นได้ หรือกระสับกระส่ายไม่สามารถอยู่นิ่งได้เหมือนที่เคยเป็นไหม"),("คุณรู้สึกไม่ดีกับตัวเอง คิดว่าตัวเองล้มเหลว หรือทำให้ตนเองหรือครอบครัวผิดหวังไหม"),("คุณคิดที่จะทำร้ายตนเอง หรือคิดว่าถ้าตายไปคงจะดี ไหม")]
 # never_answer=["เยี่ยมมากงั้นเราไปกันต่อเลยนะ","งั้นแสดงว่าคุณพักผ่อนได้เพียงพอและยังสนุกกับการทำสิ่งใหม่ๆอยู่ทุกวันแน่เลย","ดีแล้วอย่าลืมหาอะไรอร่อยๆกินด้วยนะ","ดีมากเลยคุณจะได้มีแรงเพื่อทำสิ่งที่ชอบในแต่ละวันได้อย่างเต็มที่","วันนี้คุณคงมีเรื่องสนุกๆให้ทำแน่เลย","โห แสดงว่าคุณมีความตั้งใจและมุ่งมั่นต่อสิ่งที่ทำอยู่ตลอดเวลาเลยนะ","โห แสดงว่าคุณต้องเป็นคนที่มีพลังงานเชิงบวกเยอะแน่ๆเลย","ขอให้เป้าหมายที่คุณตั้งไว้สำเร็จลุล่วง เราจะคอยเป็นกำลังใจให้คุณเอง","ดีแล้วค่ะอย่าทำอะไรอย่างงั้นเลยเราเป็นห่วงคุณนะ"]
 # sometimes_answer=["ไม่เป็นไรนะเดี๋ยวมันก็ผ่านไปนะ","อย่าลืมดื่มน้ำเยอะๆ และ อย่าลืมพักสายตาบ้างนะคะ เราเป็นห่วงคุณนะ","ลองเปลี่ยนรสชาติอาหารดูไหม จะได้ลองกินของอร่อยๆที่ไม่เคยกินด้วยน้า","ลองอ่านหนังสือหรือฟังเพลงเบาๆสบายๆดูนะจะทำให้ผ่อนคลายได้","ถ้าคุณรู้สึกเบื่อ ลองหากิจกรรมหรืองานอดิเรกที่ชอบทำไหมสิ่งเหล่านี้จะทำให้คุณหายเบื่อได้นะ","การหยุดพักจากการทำงานจะช่วยเพิ่มสมาธิได้นะ เมื่อไหร่ที่คุณทำงานเป็นเวลานาน อย่าลืมให้ตัวเองได้พักสมองบ้างเป็นบางครั้งนะ","ลองออกไปเดินเล่นข้างนอกเพื่อผ่อนคลายดูไหม มันจะทำให้คุณรู้สึกดีขึ้นนะ","ลองอดทนต่ออีกนิดดูนะ เชื่อซิว่าคุณจะค่อยๆดีขึ้น","คงไม่มีใครอยากให้เรื่องไม่ดีมาเกิดกับตัวเราหรอกนะ"]
@@ -49,7 +50,9 @@ handler = WebhookHandler('cf3954fc20dc1f7221437b5f0b811e85')
 # all_the_time_answer=["ช่วงนี้คงหนักมากสำหรับคุณ แต่ไม่เป็นไรนะ เราอยู่ข้างๆคุณเสมอ","ลองพักจากสิ่งที่ทำอยู่ แล้วออกไปสูดอากาศบริสุทธิ์ข้างนอกดูดีไหม จะได้รู้สึกสดชื่น เมื่อพักอย่างเต็มที่แล้วจะได้มีแรงทำสิ่งต่างๆได้อย่างเต็มที่","ลองเปลี่ยนบรรยากาศหรือสถานที่ในการรับประทานอาหารไหม คุณจะได้รู้สึกสนุกกับการรับประทานอาหารมากขึ้นน้า","ถ้าคุณทุกข์ใจหรือมีเรื่องที่ไม่สบายใจ อย่าเครียดไปเลยนะ ทุกปัญหามีทางออกเสมอ ปล่อยใจให้สงบตั้งสติดีๆ แล้วคุณจะผ่านไปได้ทุกเรื่องนะ","ไม่เป็นไรนะ ลองออกกำลังกาย หลับพักผ่อนให้เพียงพอ คุณจะได้รู้สึกกระปรี้กระเปร่า และอย่าลืมรับประทานอาหารอร่อยๆ และมีประโยชน์ด้วยนะคุณจะได้มีแรงในการทำกิจกรรมต่างๆได้เต็มที่","ลองออกกำลังกายก่อนที่จะต้องจดจ่อกับงาน หรือเมื่อต้องการพักสมองดูนะคะ จะทำให้เรามีสมาธิมากขึ้น หรือคุณอาจจะไปออกไปเดินสูดอากาศข้างนอกก้ได้นะ","อย่าเครียดเกินไปเลยนะคะมันมีแต่ผลไม่ดีต่อร่างกายและจิตลองหาสิ่งใหม่ๆทำให้มี  พลังทั้งกายและจิตใจเพิ่มนะ","คุณไม่ได้ตัวคนเดียวลำพังนะ ฉันอยู่ที่นี่กับคุณเอง","ฉันไม่รู้ว่าคุณไปเจออะไรมาบ้าง แต่ฉันจะไม่ทิ้งคุณและฉันจะอยู่ข้างๆคุณเองนะ"]
 g_r=0
 emotion=0
+
 #############################
+db = firestore.client()
 app = Flask(__name__)
 @app.route('/', methods=['POST']) 
 def MainFunction():
@@ -68,6 +71,7 @@ def MainFunction():
     return r
 
 def generating_answer(data_from_dialogflow_dict):
+    user_Id=data_from_dialogflow_dict["originalDetectIntentRequest"]["payload"]["data"]["source"]["userId"]
     user_text = data_from_dialogflow_dict["queryResult"]["queryText"]
     #Print intent  ที่รับมาจาก dialogflow
     print(json.dumps(data_from_dialogflow_dict, indent=4 ,ensure_ascii=False))
@@ -98,15 +102,24 @@ def generating_answer(data_from_dialogflow_dict):
     elif intent_group_question_str=="ตรวจสอบ":
         loop_check(data_from_dialogflow_dict)              
     elif intent_group_question_str=="ดู":
-                   answer_str=cal_Score()
-    
+        status=cal_Score()
+        answer_str=status
+        update_status(status,data_from_dialogflow_dict)
 
-    
-    # elif intent_group_question_str=="คุยกัน":
+    elif intent_group_question_str=="ผู้ใช้ทั้งหมด" :  
+        answer_str=show_record(data_from_dialogflow_dict)
+                 
         
+    elif intent_group_question_str=="test":
+         sen=  Chat_with_me(data_from_dialogflow_dict)
+         answer_str=sen
+    elif intent_group_question_str=="มี2":
+         answer_str=  Chat_with_me(data_from_dialogflow_dict)
+    elif intent_group_question_str=="ยินยอม2":
+          answer_str=user_info(data_from_dialogflow_dict)
        
-    else :
-        answer_str= "เราไม่เข้าใจ"
+    # else :
+    #     answer_str= "เราไม่เข้าใจ"
     answer_from_bot = {"fulfillmentText": answer_str}
             
                     #แปลงจาก dict ให้เป็น JSON
@@ -114,7 +127,12 @@ def generating_answer(data_from_dialogflow_dict):
                     
     return answer_from_bot
 
-
+def update_status(status,data):
+    user_Id=data["originalDetectIntentRequest"]["payload"]["data"]["source"]["userId"]
+    db.collection('User').document(f'{user_Id}').update({
+        u'status':status
+    }) 
+    
 
 def cal_Score():   
     global g_r
@@ -126,7 +144,7 @@ def cal_Score():
     elif 13<=score<=18:
         sum="มีการของโรคซึมเศร้า ระดับปานกลาง"
     elif score>=19:
-        sum="มีการของโรคซึมเศร้า ระดับรุนแรง"
+        sum="มีการของโรคซึมเศร้า ระดับรุนแรง" 
     return sum
 
 def upround(data_from):
@@ -144,20 +162,29 @@ def loop_check(data)  :
         upround(3)
 def Chat_with_me(input_from_user):
     userID = input_from_user["originalDetectIntentRequest"]["payload"]["data"]["source"]["userId"]
-    user_text = input_from_user["queryResult"]["queryText"]
-    Check_sentiment(user_text)
-   
-    
-def Check_sentiment(user_text):
-    global  emotion
+    user_text = input_from_user["originalDetectIntentRequest"]["payload"]["data"]["message"]["text"]
+    global emotion
     analyzed_word=str(Usesentiment.useSentiment(str(user_text)))
-    if analyzed_word == "pos":
-        emotion = "1"
-        answer_str = "ดีจัง"
-    elif analyzed_word == "neg":
-        emotion = "-1"
-        answer_str = "ไม่่เป็นไรนะ"
-    return answer_str
+    return analyzed_word
+    # if analyzed_word=="pos":
+    #     return "เป็ด"
+    # elif analyzed_word=="neg":
+    #     return "ไก่"
+def user_info(data):
+    age=data["queryResult"]["outputContexts"][1]["parameters"]["age"]
+    gender=data["queryResult"]["outputContexts"][1]["parameters"]["gender"]
+    user_Id=data["originalDetectIntentRequest"]["payload"]["data"]["source"]["userId"]
+    db.collection('User').document(f'{user_Id}').set({
+        u'userId':user_Id,
+        u'age':age,
+        u'gender':gender,
+        u'status':""
+    }) 
+    return "บันทึกข้อมูลเสร็จสิ้น"
+def show_record(data):
+    user_Id=data["originalDetectIntentRequest"]["payload"]["data"]["source"]["userId"]
+    user=db.collection('User').document(f'{user_Id}').count()
+    return user
 if __name__ == '__main__':
     app.run(debug=True)
 
