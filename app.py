3e0c6386a70dea469d2400c2b08fbb2166f8799d
Alpacz <74106021+Alp4c4@users.noreply.github.com>
2022-11-25 11:08:16 +0700

    -

--- app.py
+++ app.py
@@ -42,14 +42,8 @@ from firebase_admin import firestore
 cred=credentials.Certificate("depreesion-4eb38-firebase-adminsdk-r6lnp-402912a06a.json")
 firebase_admin.initialize_app(cred,{'storageBucket':'gs://depreesion-4eb38.appspot.com'})
 ##################################
-# question_for_test=[("ที่ผ่านมาคุณเป็นอย่างไรบ้าง รู้สึกมีเรื่องที่ไม่สบายใจหรือท้อแท้บ้างไหม"),("คุณรู้สึกเหนื่อยง่าย ไม่กระปรี้กระเปร่าบ้างไหม"),("คุณรู้สึกเบื่ออาหาร หรือกินมากเกินไปไหม"),("คุณรู้สึกว่าตัวเองหลับยาก หลับๆตื่นๆ หรือ หลับมากเกินไปไหม"),("คุณรู้สึกเบื่อบ้างไหม"),("คุณไม่มีสมาธิ เวลาทำงานที่ต้องใช้ความตั้งใจ เช่นการดูโทรทัศน์ หรือ อ่านหนังสือ ไหม"),("คุณรู้สึกว่าตัวเองพูดช้า ทำอะไรช้าลง จนคนอื่นสังเกตเห็นได้ หรือกระสับกระส่ายไม่สามารถอยู่นิ่งได้เหมือนที่เคยเป็นไหม"),("คุณรู้สึกไม่ดีกับตัวเอง คิดว่าตัวเองล้มเหลว หรือทำให้ตนเองหรือครอบครัวผิดหวังไหม"),("คุณคิดที่จะทำร้ายตนเอง หรือคิดว่าถ้าตายไปคงจะดี ไหม")]
-# never_answer=["เยี่ยมมากงั้นเราไปกันต่อเลยนะ","งั้นแสดงว่าคุณพักผ่อนได้เพียงพอและยังสนุกกับการทำสิ่งใหม่ๆอยู่ทุกวันแน่เลย","ดีแล้วอย่าลืมหาอะไรอร่อยๆกินด้วยนะ","ดีมากเลยคุณจะได้มีแรงเพื่อทำสิ่งที่ชอบในแต่ละวันได้อย่างเต็มที่","วันนี้คุณคงมีเรื่องสนุกๆให้ทำแน่เลย","โห แสดงว่าคุณมีความตั้งใจและมุ่งมั่นต่อสิ่งที่ทำอยู่ตลอดเวลาเลยนะ","โห แสดงว่าคุณต้องเป็นคนที่มีพลังงานเชิงบวกเยอะแน่ๆเลย","ขอให้เป้าหมายที่คุณตั้งไว้สำเร็จลุล่วง เราจะคอยเป็นกำลังใจให้คุณเอง","ดีแล้วค่ะอย่าทำอะไรอย่างงั้นเลยเราเป็นห่วงคุณนะ"]
-# sometimes_answer=["ไม่เป็นไรนะเดี๋ยวมันก็ผ่านไปนะ","อย่าลืมดื่มน้ำเยอะๆ และ อย่าลืมพักสายตาบ้างนะคะ เราเป็นห่วงคุณนะ","ลองเปลี่ยนรสชาติอาหารดูไหม จะได้ลองกินของอร่อยๆที่ไม่เคยกินด้วยน้า","ลองอ่านหนังสือหรือฟังเพลงเบาๆสบายๆดูนะจะทำให้ผ่อนคลายได้","ถ้าคุณรู้สึกเบื่อ ลองหากิจกรรมหรืองานอดิเรกที่ชอบทำไหมสิ่งเหล่านี้จะทำให้คุณหายเบื่อได้นะ","การหยุดพักจากการทำงานจะช่วยเพิ่มสมาธิได้นะ เมื่อไหร่ที่คุณทำงานเป็นเวลานาน อย่าลืมให้ตัวเองได้พักสมองบ้างเป็นบางครั้งนะ","ลองออกไปเดินเล่นข้างนอกเพื่อผ่อนคลายดูไหม มันจะทำให้คุณรู้สึกดีขึ้นนะ","ลองอดทนต่ออีกนิดดูนะ เชื่อซิว่าคุณจะค่อยๆดีขึ้น","คงไม่มีใครอยากให้เรื่องไม่ดีมาเกิดกับตัวเราหรอกนะ"]
-# often_answer=["ไม่เป็นไรนะไม่ต้องเครียด เดี๋ยวเรื่องแย่ๆก็ผ่านไปนะ","การออกกำลังกายและกินของที่มีประโยชน์ เช่น พวกผลไม้ จะทำให้เราสดชื่นได้น้า","ลองออกกำลังกายแบบเบาๆดูไหม มันจะช่วยกระตุ้นการอยากอาหารได้นะ","ลองออกกำลังกายอย่างสม่ำเสมอดูนะ นอกจากจะทำให้คุณนอนหลับสนิทแล้ว ก็ยังช่วยให้ร่างกายของคุณแข็งแรงอีกด้วยนะ","ลองหาเวลาเพื่อทำกิจกรรมต่างๆ ร่วมกับคนในครอบครัว หรือเพื่อนๆดูนะ","การเปิดเพลงขณะทำงานจะช่วยเรื่องสมาธิได้นะ ถึงแม้ว่าคุณจะไม่ชอบฟังเพลงขณะทำงาน แต่การใช้เสียงธรรมชาติหรือเสียงดนตรีเบาๆ อาจช่วยเพิ่มสมาธิในการทำงานได้","หายใจเข้าลึกๆตั้งสติให้ดีนะ เดี๋ยวทุกจะดีขึ้นเองนะ","อย่าโทษตัวเองเลยคุณทำได้ดีแล้วลองพยายามต่อไปนะ เราจะช่วยคุณเอง","อย่าคิดจะทำมันอะไรอย่างงั้นเลยมันไม่ดีต่อทั้งตัวคุณและคนที่คุณรักนะ"]
-# all_the_time_answer=["ช่วงนี้คงหนักมากสำหรับคุณ แต่ไม่เป็นไรนะ เราอยู่ข้างๆคุณเสมอ","ลองพักจากสิ่งที่ทำอยู่ แล้วออกไปสูดอากาศบริสุทธิ์ข้างนอกดูดีไหม จะได้รู้สึกสดชื่น เมื่อพักอย่างเต็มที่แล้วจะได้มีแรงทำสิ่งต่างๆได้อย่างเต็มที่","ลองเปลี่ยนบรรยากาศหรือสถานที่ในการรับประทานอาหารไหม คุณจะได้รู้สึกสนุกกับการรับประทานอาหารมากขึ้นน้า","ถ้าคุณทุกข์ใจหรือมีเรื่องที่ไม่สบายใจ อย่าเครียดไปเลยนะ ทุกปัญหามีทางออกเสมอ ปล่อยใจให้สงบตั้งสติดีๆ แล้วคุณจะผ่านไปได้ทุกเรื่องนะ","ไม่เป็นไรนะ ลองออกกำลังกาย หลับพักผ่อนให้เพียงพอ คุณจะได้รู้สึกกระปรี้กระเปร่า และอย่าลืมรับประทานอาหารอร่อยๆ และมีประโยชน์ด้วยนะคุณจะได้มีแรงในการทำกิจกรรมต่างๆได้เต็มที่","ลองออกกำลังกายก่อนที่จะต้องจดจ่อกับงาน หรือเมื่อต้องการพักสมองดูนะคะ จะทำให้เรามีสมาธิมากขึ้น หรือคุณอาจจะไปออกไปเดินสูดอากาศข้างนอกก้ได้นะ","อย่าเครียดเกินไปเลยนะคะมันมีแต่ผลไม่ดีต่อร่างกายและจิตลองหาสิ่งใหม่ๆทำให้มี  พลังทั้งกายและจิตใจเพิ่มนะ","คุณไม่ได้ตัวคนเดียวลำพังนะ ฉันอยู่ที่นี่กับคุณเอง","ฉันไม่รู้ว่าคุณไปเจออะไรมาบ้าง แต่ฉันจะไม่ทิ้งคุณและฉันจะอยู่ข้างๆคุณเองนะ"]
 g_r=0
 emotion=0
-# photo_url=0
 bucket=storage.bucket()
 #############################
 db = firestore.client()
@@ -57,21 +51,14 @@ collection=db.collection('User')
 app = Flask(__name__)
 @app.route('/', methods=['POST']) 
 def MainFunction():
-  
-   
     #รับ intent จาก Dailogflow
     data_from_dialogflow_raw = request.get_json(silent=True, force=True)
-
     #เรียกใช้ฟังก์ชัน generate_answer เพื่อแยกส่วนของคำถาม
     answer_from_bot = generating_answer(data_from_dialogflow_raw)
-    
     #ตอบกลับไปที่ Dailogflow
     r = make_response(answer_from_bot)
-    
     r.headers['Content-Type'] = 'application/json' #การตั้งค่าประเภทของข้อมูลที่จะตอบกลับไป
-    
     return r
-
 def generating_answer(data_from_dialogflow_dict):
     user_Id=data_from_dialogflow_dict["originalDetectIntentRequest"]["payload"]["data"]["source"]["userId"]
     response_id=data_from_dialogflow_dict["responseId"]
@@ -81,11 +68,6 @@ def generating_answer(data_from_dialogflow_dict):
     #เก็บค่าชื่อของintentที่รับมาจากdialogflow
     intent_group_question_str=data_from_dialogflow_dict["queryResult"]["intent"]["displayName"]
     #ลูปตัวเลือกของฟังชั่นสำหรับตอบคำถามกลับ
-    # if intent_group_question_str=="พร้อม":
-    #     
-    # if intent_group_question_str=="ดู":
-    #         global g_r
-    #         answer_str=cal_Score(g_r)
     if intent_group_question_str=="พร้อม2":
         loop_check(data_from_dialogflow_dict)         
     elif intent_group_question_str=="พร้อม3":
@@ -106,7 +88,6 @@ def generating_answer(data_from_dialogflow_dict):
         loop_check(data_from_dialogflow_dict)              
     elif intent_group_question_str=="ดู":
         status=cal_Score()
-        # answer_str=status
         global g_r
         g_r=0
         update_status(status,data_from_dialogflow_dict)
@@ -114,15 +95,9 @@ def generating_answer(data_from_dialogflow_dict):
     elif intent_group_question_str=="ผลการประเมิน":
         check= recheck(data_from_dialogflow_dict)
         answer_str=notifyPic(check_respone(check))
-    # elif intent_group_question_str=="ผู้ใช้ทั้งหมด" :  
-    #     answer_str=show_record(data_from_dialogflow_dict)
     elif intent_group_question_str=="คำแนะนำ1":
         url=check_advice(data_from_dialogflow_dict)
         answer_str=notifyPic(url)
-    # elif intent_group_question_str=="เล่า":
-    #     answer_str=notifyPic()
-    # elif intent_group_question_str=="มี2":
-    #      answer_str=  Chat_with_me(data_from_dialogflow_dict)
     elif intent_group_question_str=="ยินยอม2":
           answer_str=user_info(data_from_dialogflow_dict)
     # else :
@@ -215,20 +190,6 @@ def recheck(data):
     resp=res['status']
     return resp
 ######################################################################################
-
-# def Chat_with_me(input_from_user):
-#     userID = input_from_user["originalDetectIntentRequest"]["payload"]["data"]["source"]["userId"]
-#     user_text = input_from_user["originalDetectIntentRequest"]["payload"]["data"]["message"]["text"]
-#     analyzed_word=str(Usesentiment.useSentiment(str(user_text)))
-#     if analyzed_word=='pos':
-#             return "pos"
-#     elif analyzed_word=='neg':
-#             return "neg"
-
-# def show_record(data):
-#     user_Id=data["originalDetectIntentRequest"]["payload"]["data"]["source"]["userId"]
-#     user=db.collection('User').document(f'{user_Id}').count()
-#     return user
 if __name__ == '__main__':
     app.run(debug=True)
 
